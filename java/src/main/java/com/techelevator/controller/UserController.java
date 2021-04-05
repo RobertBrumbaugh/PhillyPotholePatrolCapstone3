98e1a14ea11260655b6dde79c3f4dd10d4e49e48
Samantha Keller <samanthakellerphl@gmail.com>
2021-04-05 19:28:21 -0400

    Added Controller Mapping

--- java/src/main/java/com/techelevator/controller/UserController.java
+++ java/src/main/java/com/techelevator/controller/UserController.java
@@ -2,13 +2,19 @@ package com.techelevator.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.techelevator.dao.UserDAO;
 import com.techelevator.dao.UserSqlDAO;
 import com.techelevator.model.User;
+
 
 @RestController
 public class UserController {
@@ -19,9 +25,31 @@ public class UserController {
 		this.UserSqlDAO = UserSqlDAO;
 	}
 	
-	@RequestMapping(value = "/users", method = RequestMethod.GET)
+	@RequestMapping(path = "/users", method = RequestMethod.GET)
 	public List<User> getUsers() {
 		return UserSqlDAO.findAll();
 	}
+	
+	@RequestMapping(path= "/users/{userId}", method = RequestMethod.GET)
+	public User getUserByID(@PathVariable Long userId) {
+		return UserSqlDAO.getUserById(userId);	
+	}
+	
+	@RequestMapping(path= "/users/find/{username}", method = RequestMethod.GET)
+	public User findByUsername(@PathVariable String username) {
+		return UserSqlDAO.findByUsername(username);
+	}
+	
+	@RequestMapping(path= "/users/findID/{username}", method = RequestMethod.GET)
+	public int findIdByUsername(@PathVariable String username) {
+		return UserSqlDAO.findIdByUsername(username);
+	}
+	
+	// wasn't sure how to implement this one
+//	@ResponseStatus(HttpStatus.CREATED)
+//	@RequestMapping( path = "", method = RequestMethod.POST)
+//	public boolean create(@Valid @RequestBody , @PathVariable("") ) {
+//	   return UserSqlDAO.create(username, password, role);
+//	}
 
 }
