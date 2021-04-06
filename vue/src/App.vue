0f0088b6b273fc6c70cd1dceda721b2b606312a9
Samantha Keller <samanthakellerphl@gmail.com>
2021-04-06 18:12:09 -0400

    add map

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -5,10 +5,23 @@
       <router-link v-bind:to="{ name: 'report-list' }">Current Potholes</router-link>&nbsp;|&nbsp;
       <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
     </div>
+    <GoogleMap />
     <router-view />
   </div>
 </template>
 
+<script>
+
+import GoogleMap from './components/GoogleMap.vue';
+
+export default {
+  name: 'App',
+  components: {
+    GoogleMap
+  }
+}
+
+</script>
 
 <style>
 
--- vue/src/main.js
+++ vue/src/main.js
@@ -3,10 +3,19 @@ import App from './App.vue'
 import router from './router/index'
 import store from './store/index'
 import axios from 'axios'
+import * as VueGoogleMaps from 'vue2-google-maps'
 
 Vue.config.productionTip = false
 
 axios.defaults.baseURL = process.env.VUE_APP_REMOTE_API;
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyDjR3jy_701Pqd54qSMyahGbu11u0EDZ1Y',
+    libraries: 'places',
+  }
+});
+
 
 new Vue({
   router,
