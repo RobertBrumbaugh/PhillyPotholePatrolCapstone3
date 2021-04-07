a35f390e05de7d0144a0e210cfbdf37ca70e2139
Samantha Keller <samanthakellerphl@gmail.com>
2021-04-07 18:27:08 -0400

    Updated edit report

--- vue/src/components/UpdateReport.vue
+++ vue/src/components/UpdateReport.vue
@@ -1,53 +1,64 @@
 <template>
-  <form v-on:submit.prevent>
-    <!-- <div class="field">
+<div>
+  <div v-for="report in reports" v-bind:key="report.report_id" class="report">
+      <p>
+       Report id: {{ report.report_id }}  
+       
+       Current Status: {{ report.status }} 
+       Change Status:
+        <select name="status" id="status">
+          <option value="1">Reported</option>
+          <option value="2">Inspected</option>
+          <option value="3">Repaired</option>
+      </select>
+
+      <!-- Make each of these v-model on click to the different methods -->
+
+      Current Severity: {{ report.severity }} 
+       Change Severity:
+        <select name="severity" id="severity">
+          <option value="1">Minor</option>
+          <option value="2">Could bust a tire</option>
+          <option value="3">Do not drive over me</option>
+          <option value="4">New route advised</option>
+          <option value="5">Catastrophic</option>
+      </select>
+       Date Reported:{{ report.reported }}
+      </p>
+     
+    </div>
+
+  <!-- <form v-on:submit.prevent>
+    <div class="field">
       <label for="title">Title</label>
       <input type="text" v-model="report_id" />
     </div>
     <div class="actions">
       <button type="submit" v-on:click="updateTopic()">Save Document</button>
-    </div> -->
-  </form>
+    </div>
+  </form> -->
+  </div>
 </template>
 
 
 <script>
-// import ReportService from '../services/ReportService'
-// export default {
-//     name: 'update-report',
-//     props: ["report_id"],
-//     data() {
-//         return {
-//           status: ""
-//         }
-//     },
-//     methods: {
-//         updateStatus() {
-//             const report = { id: this.report_id, status: this.status_id}
-//             ReportService.updateStatus(report_id, status_id).then((response => {
-//                 if (response.status === 200) {
-//               this.$router.push(`/`);
-//                 }
-//             }))
-//             },
-//         updateInspected() {
+import reportService from '../services/ReportService'
 
-//         },
-//         updateRepaired() {
-
-//         },
-//         updateSeverity() {
-
-//         }
-//   },
-
-
-//     created() {
-//       ReportService.list
-//     }
-// }
-
-
+export default {
+    name: 'update-report',
+    props: ["report"],
+  
+    data() {
+      return {
+        reports: [],
+      };
+    },
+    created() {
+      reportService.list().then((response) => {
+        this.reports = response.data;
+      });
+    },
+  };
 
 </script>
 
--- vue/src/views/EditReport.vue
+++ vue/src/views/EditReport.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h1>Edit Report:</h1>
+    <update-report />
     <!-- <update-report v-bind:report_id="parseInt($route.params.id)" /> -->
   </div>
 </template>
