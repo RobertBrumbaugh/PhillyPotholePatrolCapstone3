01924da948f877db0b7e411e9b56d5ac24e8af8f
Robert Brumbaugh <robertebrumbaugh@gmail.com>
2021-04-08 15:10:40 -0400

    axios/http update

--- vue/src/services/ReportService.js
+++ vue/src/services/ReportService.js
@@ -1,39 +1,39 @@
 import axios from 'axios'; 
 
-const http = axios.create({
+/* const http = axios.create({
   baseURL: "http://localhost:8080"
-});
+}); */
 
 export default {
 
     list() {
-        return http.get('/reports');
+        return axios.get('/reports');
     },
 
     // I think we need to add a get id method
 
     addReport(report) {
-      return http.post('/reports', report);
+      return axios.post('/reports', report);
     },
 
     deleteReport(report_id) {
-      return http.delete(`/reports/${report_id}`)
+      return axios.delete(`/reports/${report_id}`)
     },
     
     updateStatus(report_id, status_id) {
-      return http.put(`/reports/${report_id}/status/${status_id}`)
+      return axios.put(`/reports/${report_id}/status/${status_id}`)
     },
 
     updateInspected(report_id, inspected_date) {
-      return http.put(`/reports/${report_id}/inspected/${inspected_date}`)
+      return axios.put(`/reports/${report_id}/inspected/${inspected_date}`)
     },
 
     updateRepaired(report_id, repaired_date) {
-      return http.put(`/reports/${report_id}/repaired/${repaired_date}`)
+      return axios.put(`/reports/${report_id}/repaired/${repaired_date}`)
     },
 
     updateSeverity(report_id, severity_id) {
-      return http.put(`/reports/${report_id}/severity/${severity_id}`)
+      return axios.put(`/reports/${report_id}/severity/${severity_id}`)
     }
 
 }
