ecf0af7f667105c9371ff629f1b2d8f82acb1e4e
Robert Brumbaugh <robertebrumbaugh@gmail.com>
2021-04-13 12:49:38 -0400

    Updated seed data in DB

--- database/pothole-tables-01.sql
+++ database/pothole-tables-01.sql
@@ -27,6 +27,12 @@ INSERT INTO users (username,password_hash,role) VALUES ('JohnnyQPublic','$2a$08$
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
 INSERT INTO users (username,password_hash,role) VALUES ('employee','$2a$10$EwiZHu3h.v1KmhrCpNloEOSPlvK3fKCwehl1uHgoWh7vfsDriAFR.','ROLE_EMPLOYEE');
 INSERT INTO users (username,password_hash,role) VALUES ('SouthPhillyGirl','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
+INSERT INTO users (username,password_hash,role) VALUES ('ThisIsGritty','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
+INSERT INTO users (username,password_hash,role) VALUES ('ChzSteakChamp','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
+INSERT INTO users (username,password_hash,role) VALUES ('BenFranklinSim25','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
+INSERT INTO users (username,password_hash,role) VALUES ('SwoopSwoopFly09','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
+INSERT INTO users (username,password_hash,role) VALUES ('JawnBawnJovi','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
+
 
 
 CREATE TABLE reports
@@ -113,10 +119,18 @@ INSERT INTO status_types (status_id, status_desc)
 INSERT INTO reports (username, lat, lng, location, user_severity, reported, status, severity) 
         VALUES                                                     
         ( 'JohnnyQPublic', 39.92749552614909, -75.16582461072275, '1800 S Camac St, Philadelphia, PA 19148', 'Could bust a tire', '4/5/2021, 9:03:20 AM', 1, 0),
-        ( 'SouthPhillyGirl', 39.92646525024146, -75.15076316471925, '244 Watkins St, Philadelphia, PA 19148', 'Minor', '4/6/2021, 5:13:20 PM', 1, 0);
+        ( 'SouthPhillyGirl', 39.92646525024146, -75.15076316471925, '244 Watkins St, Philadelphia, PA 19148', 'Minor', '4/6/2021, 5:13:20 PM', 1, 0),
+        ( 'ThisIsGritty', 39.93311326475592, -75.1589863105424, '1283 S 9th St, Philadelphia, PA 19147', 'Do not drive over me', '4/10/2021, 8:13:20 AM', 1, 0),
+        ( 'ChzSteakChamp', 39.93688680567627, -75.18882593628159, '2673 Wharton St, Philadelphia, PA 19146', 'Minor', '4/8/2021, 2:30:19 PM', 1, 0),
+        ( 'BenFranklinSim25', 39.961670590112604, -75.15602882276968, '1034 Spring Garden St, Philadelphia, PA 19123', 'Do not drive over me', '4/10/2021, 8:13:20 AM', 1, 0),
+        ( 'SwoopSwoopFly09', 39.9826454810186, -75.18836681957112, '33rd St & Cecil B Moore Av, Philadelphia, PA 19121', 'New route advised', '4/7/2021, 7:45:12 AM', 1, 0),
+        ( 'JawnBawnJovi', 39.97032828538133, -75.13011447561469, '411 E Girard Ave, Philadelphia, PA 19125', 'Catastrophic', '4/13/2021, 8:13:20 AM', 1, 0);
+        
+        
+        
         
 INSERT INTO damage_claim (report_id, full_name, phone_number, email, address, incident_date, car, damage_description) 
-        VALUES (1, 'Johnny Q Public', '215-555-5555', 'fake@obvious.duh', '123 Fake Street Hollywood CA 90210', '4/5/2021', 'Toyota Corolla', 'Damage to the front passenger side wheel and axle');
+        VALUES (1, 'Johnny Q Public', '215-555-5555', 'bandit@jmail.com', '123 Fake Street Philadelphia PA 19147', '4/5/2021', 'Toyota Corolla', 'Damage to the front passenger side wheel and axle');
 
 GRANT ALL
 ON ALL TABLES IN SCHEMA public
